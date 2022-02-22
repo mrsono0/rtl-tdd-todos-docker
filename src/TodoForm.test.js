27b011961d0df34ddbadf3cec2ea702a9d803d46
mrsono0 <mrsono0@gmail.com>
2022-02-22 06:06:00 +0000

    TodoItem click event test ok

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -2,30 +2,30 @@ import React from "react";
 import { fireEvent, render } from "@testing-library/react"; //react-testing-library;
 import TodoFrom from "./TodoForm";
 
-describe('<TodoFrom />', () => {
+describe("<TodoFrom />", () => {
     const setup = (props = {}) => {
         const utils = render(<TodoFrom {...props} />);
         const { getByText, getByPlaceholderText } = utils;
-        const input = getByPlaceholderText('할 일을 입력하세요~');
-        const button = getByText('등록');
-        return {...utils, input, button};
-    }
-    it('has input and a button', () => {
-        const {input, button} = setup();
+        const input = getByPlaceholderText("할 일을 입력하세요~");
+        const button = getByText("등록");
+        return { ...utils, input, button };
+    };
+    it("has input and a button", () => {
+        const { input, button } = setup();
         expect(input).toBeTruthy();
         expect(button).toBeTruthy();
-    })
-    it('change input', () => {
-        const {input} = setup();
-        fireEvent.change(input, {target: {value: 'TDD 배우기'}});
-        expect(input).toHaveAttribute('value', 'TDD 배우기');
-    })
-    it('calls onInsert and clears input', () => {
+    });
+    it("change input", () => {
+        const { input } = setup();
+        fireEvent.change(input, { target: { value: "TDD 배우기" } });
+        expect(input).toHaveAttribute("value", "TDD 배우기");
+    });
+    it("calls onInsert and clears input", () => {
         const onInsert = jest.fn();
-        const {input, button} = setup({onInsert});
-        fireEvent.change(input, {target: {value: 'TDD 배우기'}});
+        const { input, button } = setup({ onInsert });
+        fireEvent.change(input, { target: { value: "TDD 배우기" } });
         fireEvent.click(button);
-        expect(onInsert).toBeCalledWith('TDD 배우기');
-        expect(input).toHaveAttribute('value', '');
-    })
-})
+        expect(onInsert).toBeCalledWith("TDD 배우기");
+        expect(input).toHaveAttribute("value", "");
+    });
+});
--- src/TodoItem.test.js
+++ src/TodoItem.test.js
@@ -1,25 +1,41 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import TodoItem from "./TodoItem";
 
-describe('<TodoItem />', () => {
+describe("<TodoItem />", () => {
     const sampleTodo = {
         id: 1,
-        text: 'TDD 배우기',
-        done: false
+        text: "TDD 배우기",
+        done: false,
     };
     const setup = (props = {}) => {
-        const initialProps = {todo: sampleTodo}; 
+        const initialProps = { todo: sampleTodo };
         const utils = render(<TodoItem {...initialProps} {...props} />);
         const { getByText } = utils;
         const todo = props.todo || initialProps.todo;
-        const button = getByText('삭제');
+        const button = getByText("삭제");
         const span = getByText(todo.text);
-        return {...utils, span, button};
+        return { ...utils, span, button };
     };
-    it('has span and button', () => {
-        const {span, button} = setup();
+    it("has span and button", () => {
+        const { span, button } = setup();
         expect(span).toBeTruthy();
         expect(button).toBeTruthy();
     });
-})
+    it("does not show line-through on span when done is false", () => {
+        const { span } = setup({ todo: { ...sampleTodo, done: false } });
+        expect(span).not.toHaveStyle("text-decoration: line-through;");
+    });
+    it("calls onToggle", () => {
+        const onToggle = jest.fn();
+        const { span } = setup({ onToggle });
+        fireEvent.click(span);
+        expect(onToggle).toBeCalledWith(sampleTodo.id);
+    });
+    it("calls onRemove", () => {
+        const onRemove = jest.fn();
+        const { button } = setup({ onRemove });
+        fireEvent.click(button);
+        expect(onRemove).toBeCalledWith(sampleTodo.id);
+    });
+});
