e93265daa1cd978db71a9bc6e4d2faf8894bd43d
mrsono0 <mrsono0@gmail.com>
2022-02-22 02:27:30 +0000

    TodoFrom Input test ok

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react"; //react-testing-library;
+import { fireEvent, render } from "@testing-library/react"; //react-testing-library;
 import TodoFrom from "./TodoForm";
 
 describe('<TodoFrom />', () => {
@@ -7,5 +7,11 @@ describe('<TodoFrom />', () => {
         const {getByText, getByPlaceholderText} = render(<TodoFrom />);
         getByPlaceholderText('할 일을 입력하세요~'); //input 있는 지 없는지
         getByText('등록'); // button이 있는지 없는지
+    })
+    it('change input', () => {
+        const {getByPlaceholderText} = render(<TodoFrom />);
+        const input = getByPlaceholderText('할 일을 입력하세요~');
+        fireEvent.change(input, {target: {value: 'TDD 배우기'}});
+        expect(input).toHaveAttribute('value', 'TDD 배우기');
     })
 })
