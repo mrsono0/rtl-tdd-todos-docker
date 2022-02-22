cc6f8f318211b942e294f93995d581eb37977a4a
mrsono0 <mrsono0@gmail.com>
2022-02-22 03:19:28 +0000

    TodoFrom refactoring ok

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -3,22 +3,26 @@ import { fireEvent, render } from "@testing-library/react"; //react-testing-libr
 import TodoFrom from "./TodoForm";
 
 describe('<TodoFrom />', () => {
+    const setup = (props = {}) => {
+        const utils = render(<TodoFrom {...props} />);
+        const { getByText, getByPlaceholderText } = utils;
+        const input = getByPlaceholderText('할 일을 입력하세요~');
+        const button = getByText('등록');
+        return {...utils, input, button};
+    }
     it('has input and a button', () => {
-        const {getByText, getByPlaceholderText} = render(<TodoFrom />);
-        getByPlaceholderText('할 일을 입력하세요~'); //input 있는 지 없는지
-        getByText('등록'); // button이 있는지 없는지
+        const {input, button} = setup();
+        expect(input).toBeTruthy();
+        expect(button).toBeTruthy();
     })
     it('change input', () => {
-        const {getByPlaceholderText} = render(<TodoFrom />);
-        const input = getByPlaceholderText('할 일을 입력하세요~');
+        const {input} = setup();
         fireEvent.change(input, {target: {value: 'TDD 배우기'}});
         expect(input).toHaveAttribute('value', 'TDD 배우기');
     })
     it('calls onInsert and clears input', () => {
         const onInsert = jest.fn();
-        const { getByText, getByPlaceholderText} = render(<TodoFrom onInsert={onInsert} />);
-        const input = getByPlaceholderText('할 일을 입력하세요~');
-        const button = getByText('등록');
+        const {input, button} = setup({onInsert});
         fireEvent.change(input, {target: {value: 'TDD 배우기'}});
         fireEvent.click(button);
         expect(onInsert).toBeCalledWith('TDD 배우기');
