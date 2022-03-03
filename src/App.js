805e436b1486c2bd88ad28f1ec08f40051adba5d
mrsono0 <mrsono0@gmail.com>
2022-03-03 08:23:50 +0000

    code review todo insert id test ok

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TodoApp from "./TodoApp";
 function App() {
     return (
         <div className="App">
+            <h1>Code Review Todo Inser Id</h1>
             <h2>CI CD jenkins with github auto deploy</h2>
             <TodoApp />
         </div>
